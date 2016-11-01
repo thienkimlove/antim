c4b6b72a6d52efcfc393063b58515ba9313b2294
thienkimlove <thienkimlove@gmail.com>
2016-11-02 00:08:41 +0700

    init

---
+++ resources/views/mails/question.blade.php
@@ -0,0 +1,12 @@
+<p>
+    Người hỏi : {{$data['ask_person']}}
+</p>
+<p>
+    Địa chỉ : {{$data['ask_address']}}
+</p>
+<p>
+    Số điện thoại : {{$data['ask_phone']}}
+</p>
+<p>
+    Nội dung câu hỏi : {{$data['question']}}
+</p>
