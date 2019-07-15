094734d3a41f8649d832dfeaf48ca1ccf5e431b1
Quan Do <thienkimlove@gmail.com>
2019-07-15 14:14:54 +0700

    update

--- public/bower_components/ckeditor/config.js
+++ public/bower_components/ckeditor/config.js
@@ -1,11 +1,11 @@
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	config.filebrowserBrowseUrl = '/js/admin/kcfinder/browse.php?opener=ckeditor&type=files';
-	config.filebrowserImageBrowseUrl = '/js/admin/kcfinder/browse.php?opener=ckeditor&type=images';
-	config.filebrowserFlashBrowseUrl = '/js/admin/kcfinder/browse.php?opener=ckeditor&type=flash';
-	config.filebrowserUploadUrl = '/js/admin/kcfinder/upload.php?opener=ckeditor&type=files';
-	config.filebrowserImageUploadUrl = '/js/admin/kcfinder/upload.php?opener=ckeditor&type=images';
-	config.filebrowserFlashUploadUrl = '/js/admin/kcfinder/upload.php?opener=ckeditor&type=flash';
+	config.filebrowserBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=files';
+	config.filebrowserImageBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=images';
+	config.filebrowserFlashBrowseUrl = '/kcfinder/browse.php?opener=ckeditor&type=flash';
+	config.filebrowserUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=files';
+	config.filebrowserImageUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=images';
+	config.filebrowserFlashUploadUrl = '/kcfinder/upload.php?opener=ckeditor&type=flash';
 	//do not add extra paragraph to html
 	config.autoParagraph = false;
 };
